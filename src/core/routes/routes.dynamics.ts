30f80cc182e5a5318d429dde53b26fd3386db1ce
MaxC0d3 <maximitianoreste@gmail.com>
2023-08-05 19:19:18 -0500

    refactor code

--- src/core/routes/routes.dynamics.ts
+++ src/core/routes/routes.dynamics.ts
@@ -1,51 +1,51 @@
-import { Application } from 'express'
+import { Application, Request, Response } from 'express'
 import * as fs from 'fs'
 import { ENV_CONFIG } from '../../config/env.config'
 
-interface RoutesDynamicsI {
+interface RouteClass {
+  (req: Request, res: Response): void
+}
+
+interface RouteData {
   routeUrl: string
-  routeClass: any
+  routeClass: RouteClass
 }
 
 export class RoutesDynamics {
-  private readonly routesUrl: any[] = []
-  private routesRead: RoutesDynamicsI[] = []
+  private readonly routesUrl: string[] = []
+  private routesRead: RouteData[] = []
   private _apiPrefix: string = ENV_CONFIG.API_PREFIX
   private _apiVersion: string = ENV_CONFIG.API_VERSION
 
   constructor(private readonly app: Application) {
     this.routesUrl = fs.readdirSync(`${__dirname}/_routes`)
+    this.loadRoutes()
+  }
 
-    this.routesUrl.forEach((routeUrl: string) => {
+  private async importRouteClass(routeUrl: string): Promise<RouteClass> {
+    const routeModule = await import(`${__dirname}/_routes/${routeUrl}`)
+    return routeModule.default
+  }
+
+  private loadRoutes(): void {
+    this.routesUrl.forEach(async (routeUrl: string) => {
       try {
         const routeUrlReplaced = routeUrl.replace('.ts', '')
-        const route = import(`${__dirname}/_routes/${routeUrl}`)
-
-        const routeClass = route.then((route: any) => route.default).then((route: any) => route)
-
+        const routeClass = await this.importRouteClass(routeUrl)
         this.routesRead.push({
           routeUrl: routeUrlReplaced,
           routeClass,
         })
+
+        const apiRoute = `/${this._apiPrefix}/${this._apiVersion}/${routeUrlReplaced}`
+        console.log(apiRoute)
+
+        this.app.use(apiRoute, (req, res) => {
+          routeClass(req, res)
+        })
       } catch (err) {
         console.error(err)
       }
-    })
-
-    this.readRoutes()
-  }
-
-  public readRoutes(): void {
-    this.routesRead.forEach((route: any) => {
-      const routeExtracted = route.routeClass.then((route: any) => route)
-
-      const apiRoute = `/${this._apiPrefix}/${this._apiVersion}/${route.routeUrl}`
-
-      console.log(apiRoute)
-
-      this.app.use(`/${this._apiPrefix}/${this._apiVersion}/${route.routeUrl}`, (req, res) => {
-        routeExtracted.then((route: any) => route(req, res))
-      })
     })
   }
 }
